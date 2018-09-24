75dea114e9a9e2d4e5eecf2e741494bdb9acb311
tombshaw <43508967+tombshaw@users.noreply.github.com>
2018-09-23 23:29:45 -0700

    clean-up, additional IMU data
    -consolidated declarations.
    -added gyro and mag data for IMU

--- extractData.py
+++ extractData.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Sep 23 16:15:09 2018
-
 CS 50100 Group 16
 Python script to extract raw drone data from Mission Planner output
 
@@ -18,18 +15,13 @@ maxRow = sheet.max_row
 
 print("%i number of rows found" % (maxRow))
 
-att_time = []
-att_time_boot_ms = []
-att_pitch = []
-att_roll = []
-att_yaw = []
-att_p = []
-att_q = []
-att_r = []
-imu_time = []
-imu_xacc = []
-imu_yacc = []
-imu_zacc = []
+#attitude data
+att_time, att_time_boot_ms, att_pitch, att_roll, att_yaw, att_p, att_q, att_r = [],[],[],[],[],[],[],[]
+att_header = ["attitude time","time boot ms","roll angle","pitch angle","yaw angle","roll rate","pitch rate","yaw rate"]
+
+#raw imu data
+imu_time, imu_xacc, imu_yacc, imu_zacc, imu_xgyro, imu_ygyro, imu_zgyro, imu_xmag, imu_ymag, imu_zmag = [],[],[],[],[],[],[],[],[],[]
+imu_header = ["IMU_time","Xaccel","Yaccel","Zacce","XGyro","YGyro","ZGyro","XMag","YMag","ZMag"]
 
 for row in range(1,maxRow+1):
     if(sheet.cell(row=row,column=10).value == "mavlink_attitude_t"):
@@ -46,7 +38,12 @@ for row in range(1,maxRow+1):
         imu_xacc.append(sheet.cell(row=row,column=14).value)
         imu_yacc.append(sheet.cell(row=row,column=16).value)
         imu_zacc.append(sheet.cell(row=row,column=18).value)        
-        
+        imu_xgyro.append(sheet.cell(row=row,column=20).value)
+        imu_ygyro.append(sheet.cell(row=row,column=22).value)
+        imu_zgyro.append(sheet.cell(row=row,column=24).value)  
+        imu_xmag.append(sheet.cell(row=row,column=26).value)
+        imu_ymag.append(sheet.cell(row=row,column=28).value)
+        imu_zmag.append(sheet.cell(row=row,column=30).value)          
         
         
 #Create new worksheet and output data
@@ -57,19 +54,12 @@ ws_att.title = "ATTITUDE"
 ws_imu = book.create_sheet("IMU")
 
 #create headers
-ws_att.cell(row=1,column=1).value = "attitude time"
-ws_att.cell(row=1,column=1).value = "time boot ms"
-ws_att.cell(row=1,column=2).value = "roll angle"
-ws_att.cell(row=1,column=3).value = "pitch angle"
-ws_att.cell(row=1,column=4).value = "yaw angle"
-ws_att.cell(row=1,column=5).value = "roll rate"
-ws_att.cell(row=1,column=6).value = "pitch rate"
-ws_att.cell(row=1,column=7).value = "yaw rate"
+for i in range(len(att_header)):
+    ws_att.cell(row=1,column=i+1).value = att_header[i]
 
-ws_imu.cell(row=1,column=1).value = "IMU time"
-ws_imu.cell(row=1,column=2).value = "IMU XAccel"
-ws_imu.cell(row=1,column=3).value = "IMU YAccel"
-ws_imu.cell(row=1,column=4).value = "IMU Zaccel"
+for i in range(len(imu_header)):
+    ws_imu.cell(row=1,column=i+1).value = imu_header[i]
+
 
 for row in range(2,len(att_time)+2):
     dataindex = row-2
@@ -88,5 +78,11 @@ for row in range(2,len(imu_time)+2):
     ws_imu.cell(row=row,column=2).value = imu_xacc[dataindex]    
     ws_imu.cell(row=row,column=3).value = imu_yacc[dataindex] 
     ws_imu.cell(row=row,column=4).value = imu_zacc[dataindex] 
+    ws_imu.cell(row=row,column=5).value = imu_xgyro[dataindex]    
+    ws_imu.cell(row=row,column=6).value = imu_ygyro[dataindex] 
+    ws_imu.cell(row=row,column=7).value = imu_zgyro[dataindex] 
+    ws_imu.cell(row=row,column=8).value = imu_xmag[dataindex]    
+    ws_imu.cell(row=row,column=9).value = imu_ymag[dataindex] 
+    ws_imu.cell(row=row,column=10).value = imu_zmag[dataindex] 
     
 book.save("NewData.xlsx")
